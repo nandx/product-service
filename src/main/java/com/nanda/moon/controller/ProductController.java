66ccc814d99eae3f62a01f91d0f89a5fbf726070
nanda <nandaceria@gmail.com>
2020-08-13 21:47:30 +0700

    Swagger

--- src/main/java/com/nanda/moon/controller/ProductController.java
+++ src/main/java/com/nanda/moon/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nanda.moon.dto.ProductParamDTO;
+import com.nanda.moon.dto.ResponseDTO;
 import com.nanda.moon.service.ProductService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,21 +28,24 @@ public class ProductController {
 	private ProductService productService;
 
 	@Operation(summary = "Add Product")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Found the book", content = {
-					@Content(mediaType = "application/json", schema = @Schema(implementation = String.class)) }),
-			@ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
-			@ApiResponse(responseCode = "404", description = "Book not found", content = @Content) })
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Add Product", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }) })
 	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> addProduct(@RequestBody ProductParamDTO productParamDTO) {
 		return productService.add(productParamDTO);
 	}
 
+	@Operation(summary = "Get Product")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Get Product", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }) })
 	@RequestMapping(value = "/get/{productcode}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> getProduct(@PathVariable("productcode") String productcode) {
 		return productService.getProduct(productcode);
 	}
 
+	@Operation(summary = "Search Product")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Search Product", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }) })
 	@RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> search(@RequestParam(name = "keyword", required = false) String keyword,
 			@RequestParam(name = "page", required = false, defaultValue = "1") Integer currentpage,
